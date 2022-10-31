4d4469cd97944a59a22bef761762339fd1a57348
stkhwu <114454156+stkhwu@users.noreply.github.com>
2022-10-31 11:07:31 +0800

    Update

--- src/main/java/com/leetcode/PlusOne.java
+++ src/main/java/com/leetcode/PlusOne.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class PlusOne {
   public static void main(String[] args) {
-    int[] digits = new int[] { 9,9,9,9,9,9 };
+    int[] digits = new int[] { 9,9 };
 
     /*
      * String digit = "";
