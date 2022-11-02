ef8d0feeb8223d9980a173ba1c89087d73d880d6
stkhwu <114454156+stkhwu@users.noreply.github.com>
2022-11-02 10:11:09 +0800

    Update

--- src/main/java/com/algoexpert/SortedSquaredArray.java
+++ src/main/java/com/algoexpert/SortedSquaredArray.java
@@ -1,9 +1,7 @@
 package com.algoexpert;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SortedSquaredArray {
   public static void main(String[] args) {
