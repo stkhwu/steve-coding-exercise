91852eefdea6e531f1863625bd88b5315c864f28
stkhwu <114454156+stkhwu@users.noreply.github.com>
2022-12-29 14:29:30 +0800

    Update

--- spring-boot-example/DataModifyingViaJpa/student/src/main/resources/DML_STUDENTS.sql
+++
@@ -1,3 +0,0 @@
-delete from students where id = 1;
-
-insert into students(id,name,height) values(1, 'Tommy', 140.0);
--- spring-boot-example/TestCase/greeting/src/main/java/com/vtxlab/greeting/repository/GreetingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vtxlab.greeting.repository;
-
-import com.vtxlab.greeting.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GreetingRepository extends JpaRepository<Book,Long> {
-
-}
