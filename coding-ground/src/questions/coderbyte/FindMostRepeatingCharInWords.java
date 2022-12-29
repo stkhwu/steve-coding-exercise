683408b0d74517bfcfde70f05a9a6b60948cd35b
stkhwu <114454156+stkhwu@users.noreply.github.com>
2022-12-29 14:24:32 +0800

    Update

--- coding-ground/src/questions/coderbyte/FindMostRepeatingCharInWords.java
+++ coding-ground/src/questions/coderbyte/FindMostRepeatingCharInWords.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SomeAlgo1 {
+public class FindMostRepeatingCharInWords {
 
 
     public static void main(String[] args) {
